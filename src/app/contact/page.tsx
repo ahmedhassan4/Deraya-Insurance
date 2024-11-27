a39ef420f7b65a37c25e92fce3f35ff311c2befc
ahmedhassan4 <158503741+ahmedhassan4@users.noreply.github.com>
2024-11-27 15:01:38 +0200

    update left section for all pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,15 +4,16 @@ import { CardSection } from "@/components/layouts/card-layout/CardSection";
 import React from "react";
 import Contact from "./Contact";
 import ContactInfo from "./ContactInfo";
+import { LeftSection } from "@/components/layouts/card-layout/LeftSection";
 
 function PlanPage() {
   return (
     <CardLayout>
-      <CardSection className="bg-[#B5BE34] bg-[url('/Main.png')] bg-cover rounded-lg bg-center py-12 px-[72px] ">
+      <LeftSection>
         <CardInfo displaySocialIcons={false}>
           <ContactInfo />
         </CardInfo>
-      </CardSection>
+      </LeftSection>
       <CardSection className=" px-6 py-12 flex items-start justify-center">
         <Contact />
       </CardSection>
--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -4,15 +4,16 @@ import { CardSection } from "@/components/layouts/card-layout/CardSection";
 import React from "react";
 import ServiceInfo from "./ServiceInfo";
 import MultiStepForm from "./MultiStepForm";
+import { LeftSection } from "@/components/layouts/card-layout/LeftSection";
 
 function SubscriptionPage() {
   return (
     <CardLayout>
-      <CardSection className="bg-[#B5BE34] bg-[url('/Main.png')] bg-cover rounded-lg bg-center py-12 px-[72px] hidden lg:block">
+      <LeftSection>
         <CardInfo displaySocialIcons={false}>
           <ServiceInfo />
         </CardInfo>
-      </CardSection>
+      </LeftSection>
       <CardSection className=" px-6 py-12 flex items-center justify-center">
         <MultiStepForm />
       </CardSection>
