10d4a556134295f6629d542c4f03df2640ef5586
ahmedhassan4 <158503741+ahmedhassan4@users.noreply.github.com>
2024-11-27 11:28:09 +0200

    update contact page

---
+++ src/app/contact/ContactInfo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, Title } from "rizzui";
+
+function ContactInfo() {
+  return (
+    <div>
+      <Title className="text-white"> Get in Touch </Title>
+      <Text className="text-white mt-4">
+        {" "}
+        New partners instead of new competitors. We’re always looking to expand
+        our business across all lines of insurance. In our experience,
+        partnering with individual brokers to provide the best possible service
+        to prospect clients is always a win-win strategy.
+      </Text>
+    </div>
+  );
+}
+
+export default ContactInfo;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,15 +2,15 @@ import CardInfo from "@/components/CardInfo";
 import CardLayout from "@/components/layouts/card-layout/CardLayout";
 import { CardSection } from "@/components/layouts/card-layout/CardSection";
 import React from "react";
-import ServiceInfo from "../subscription/ServiceInfo";
 import Contact from "./Contact";
+import ContactInfo from "./ContactInfo";
 
 function PlanPage() {
   return (
     <CardLayout>
       <CardSection className="bg-[#B5BE34] bg-[url('/Main.png')] bg-cover rounded-lg bg-center py-12 px-[72px] ">
         <CardInfo displaySocialIcons={false}>
-          <ServiceInfo />
+          <ContactInfo />
         </CardInfo>
       </CardSection>
       <CardSection className=" px-6 py-12 flex items-start justify-center">
