a5fa9d6b1334b264233b3d224117ad238cd35d1a
ahmedhassan4 <158503741+ahmedhassan4@users.noreply.github.com>
2024-12-01 14:29:57 +0200

    install intel for localizaiton

---
+++ src/app/[local]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useTranslations } from "next-intl";
+
+export default function Home() {
+  const t = useTranslations("HomePage");
+  return (
+    <>
+      <h1>{t("title")}</h1>
+    </>
+  );
+}
