cfc2afdf4a6909252c9478885f13e6ca897f73bd
ahmedhassan4 <158503741+ahmedhassan4@users.noreply.github.com>
2024-12-01 20:00:24 +0200

    update service localization

--- src/app/[local]/services/DrayaServicesInfo.tsx
+++ src/app/[local]/services/DrayaServicesInfo.tsx
@@ -1,17 +1,13 @@
+import { useTranslations } from "next-intl";
 import React from "react";
 import { Text, Title } from "rizzui";
 
 function DrayaServicesInfo() {
+  const t = useTranslations("services");
   return (
     <div>
-      <Title className="text-white"> Select your Service </Title>
-      <Text className="text-white mt-4">
-        {" "}
-        We specialize in curating personalized insurance solutions that meet
-        your specific needs. Whether you&apos;re looking for medical, life, or
-        general coverage, our expert team ensures you get the best, most
-        affordable packages available.
-      </Text>
+      <Title className="text-white"> {t("title")}</Title>
+      <Text className="text-white mt-4">{t("desc")}</Text>
     </div>
   );
 }
