b2f99fce72a83aafc614f1157ed9dd065de6200a
ahmedhassan4 <158503741+ahmedhassan4@users.noreply.github.com>
2024-12-10 15:24:01 +0200

    udpate calendar

--- src/app/[locale]/subscription/Form.tsx
+++ src/app/[locale]/subscription/Form.tsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
 import { Input, Radio } from "rizzui";
-import DatePicker from "react-datepicker";
+
+import { Calendar as CalendarIcon } from "lucide-react";
+import { format, getMonth, getYear, setMonth, setYear } from "date-fns";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 import ReactFlagsSelect from "react-flags-select";
 import { useTranslations } from "next-intl";
-
-import "react-datepicker/dist/react-datepicker.css";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export const NameField = () => {
   const {
@@ -131,12 +147,39 @@ export const InterestedInField = () => {
   );
 };
 
-export const DateField = () => {
+interface DatePickerProps {
+  startYear?: number;
+  endYear?: number;
+}
+
+export function DateField({
+  startYear = getYear(new Date()) - 100,
+  endYear = getYear(new Date()) + 100,
+}: DatePickerProps) {
   const {
     control,
     formState: { errors },
   } = useFormContext();
   const t = useTranslations("subscription");
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const years = Array.from(
+    { length: endYear - startYear + 1 },
+    (_, i) => startYear + i
+  );
 
   return (
     <div className="mt-4 mb-2">
@@ -146,29 +189,115 @@ export const DateField = () => {
       <Controller
         name="date"
         control={control}
-        // You can add validation rules if needed.
-        // For example, if the date is required:
-        // rules={{ required: t("error.required") }}
-        render={({ field }) => (
-          <DatePicker
-            selected={field.value}
-            onChange={(date) => field.onChange(date)}
-            dateFormat="yyyy-MM-dd"
-            className=" border-gray-200 border-2 rounded-md  px-3 py-3 w-full focus:ring-[#B5BE34] focus:border-none"
-            wrapperClassName="!w-full !focus:ring-0 !focus:outline-none "
-            placeholderText={t("placeholder.date")}
-          />
-        )}
-      />
+        render={({ field }) => {
+          const { value, onChange } = field;
 
-      {errors.date && (
-        <p className="text-sm mt-2 text-red-500">
-          {String(errors.date.message)}
-        </p>
-      )}
+          const handleMonthChange = (month: string) => {
+            const newDate = setMonth(
+              value || new Date(),
+              months.indexOf(month)
+            );
+            onChange(newDate);
+          };
+
+          const handleYearChange = (year: string) => {
+            const newDate = setYear(value || new Date(), parseInt(year));
+            onChange(newDate);
+          };
+
+          return (
+            <>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-full py-[23px] justify-start text-left font-normal",
+                      !value && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {value ? (
+                      format(value, "PPP")
+                    ) : (
+                      <span>{t("placeholder.date")}</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <div className="flex justify-between p-2 gap-2">
+                    <Select
+                      onValueChange={handleMonthChange}
+                      value={
+                        value
+                          ? months[getMonth(value)]
+                          : months[getMonth(new Date())]
+                      }
+                    >
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Month" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {months.map((month) => (
+                          <SelectItem
+                            key={month}
+                            value={month}
+                            // className="bg-"
+                          >
+                            {month}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <Select
+                      onValueChange={handleYearChange}
+                      value={
+                        value
+                          ? getYear(value).toString()
+                          : getYear(new Date()).toString()
+                      }
+                    >
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Year" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {years.map((year) => (
+                          <SelectItem
+                            key={year}
+                            value={year.toString()}
+                            // className="bg-white z-50"
+                          >
+                            {year}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  <Calendar
+                    mode="single"
+                    selected={value}
+                    onSelect={(selectedDate) => onChange(selectedDate)}
+                    initialFocus
+                    className="-z-30"
+                    month={value || new Date()}
+                    onMonthChange={(newDate) => onChange(newDate)}
+                  />
+                </PopoverContent>
+              </Popover>
+              {errors.date && (
+                <p className=" text-sm mt-2 text-red-500">
+                  {String(errors.date.message)}
+                </p>
+              )}
+            </>
+          );
+        }}
+      />
     </div>
   );
-};
+}
+
 export const CountryField = () => {
   const {
     setValue,
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border-2 border-input bg-background hover:bg-accent hover:text-accent-foreground focus:border-[#B5BE34]",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -31,7 +31,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
