e95f755617598ce830de5601b19c59fa20b2d927
ahmednnasser11 <Ahmednnasser11@gmail.com>
2024-12-16 16:45:20 +0200

    `Added import of style.css and added className to SimpleBar component`

--- src/components/layouts/card-layout/CardLayout.tsx
+++ src/components/layouts/card-layout/CardLayout.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import cn from "@/utils/class-names";
 import SimpleBar from "simplebar-react";
-
+import "./style.css";
 export interface CardLayoutProps {
   children: React.ReactNode;
   className?: string;
@@ -13,7 +13,8 @@ const CardLayout: React.FC<CardLayoutProps> = ({ children, className }) => {
       <div
       className="p-5 h-screen"
       >
-        <SimpleBar style={{ height: "100%" }}>
+        <SimpleBar 
+         className="h-screen">
           <div
             className={cn(
               "w-full h-full grid grid-cols-1 lg:grid-cols-2 bg-white rounded-lg shadow-container overflow-hidden",
