a6bb9e43618905a7a514017e82627416a0c0d3d4
ahmednnasser11 <Ahmednnasser11@gmail.com>
2024-12-16 12:27:59 +0200

    `Update LeftSection component styles: changed py-12 to py-8`

--- src/components/layouts/card-layout/LeftSection.tsx
+++ src/components/layouts/card-layout/LeftSection.tsx
@@ -44,7 +44,7 @@ export const LeftSection: React.FC<LeftSectionProps> = ({
           "max-lg:fixed max-lg:inset-y-0 max-lg:left-0 max-lg:w-4/5 max-lg:shadow-2xl max-lg:bg-[#B5BE34]",
           isDrawerOpen ? "max-lg:translate-x-0" : "max-lg:-translate-x-full",
           // Default styling from original component
-          "bg-[#B5BE34] bg-[url('/Main.png')] bg-cover rounded-lg bg-center py-12 px-[72px]",
+          "bg-[#B5BE34] bg-[url('/Main.png')] bg-cover rounded-lg bg-center py-8 px-[72px]",
           className
         )}
       >
